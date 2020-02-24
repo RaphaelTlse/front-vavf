d20420a2fdf5419001d8767f20f8db264f686f5d
loljambe <33934964+loljambe@users.noreply.github.com>
2020-02-24 03:25:39 +0100

    Dashboard login now retrieves token from API and stores it in local storage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // in src/App.js
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import { LogsList, LogsEdit, LogsCreate } from './logs';
-import { MachinesList } from './machines';
-import { Map } from './Map';
-import { Stats } from './Stats';
-import { AdminPage } from './AdminPage';
-import { Home } from './Home';
+import {fetchUtils, Admin, Resource} from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+import {LogsList, LogsEdit, LogsCreate} from './logs';
+import {MachinesList} from './machines';
+import {Map} from './Map';
+import {Stats} from './Stats';
+import {AdminPage} from './AdminPage';
+import {Home} from './Home';
 import authProvider from './authProvider';
-import jsonServerProvider from 'ra-data-json-server';
 import MachinesIcon from '@material-ui/icons/DirectionsRailway';
 import LogsIcon from '@material-ui/icons/MenuBook';
 import MapIcon from '@material-ui/icons/Map';
@@ -16,17 +16,24 @@ import StatsIcon from '@material-ui/icons/TrendingUp';
 import AdminPageIcon from '@material-ui/icons/PermIdentity';
 import HomeIcon from '@material-ui/icons/Home';
 
-
-const dataProvider = jsonServerProvider('https://my-json-server.typicode.com/loljambe/testeip');
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({Accept: 'application/json'});
+    }
+    const token = localStorage.getItem('token');
+    options.headers.set('Authorization', `Bearer ${token}`);
+    return fetchUtils.fetchJson(url, options);
+};
+const dataProvider = simpleRestProvider('http://localhost:3000', httpClient);
 
 const App = () => (
     <Admin dataProvider={dataProvider} authProvider={authProvider}>
-        <Resource name="Accueil" list={Home} icon={HomeIcon} />
-        <Resource name="Machines" list={MachinesList} icon={MachinesIcon} />
-        <Resource name="Logs" list={LogsList} edit={LogsEdit} create={LogsCreate} icon={LogsIcon} />
-        <Resource name="Carte" list={Map} icon={MapIcon} />
-        <Resource name="Statistiques" list={Stats} icon={StatsIcon} />
-        <Resource name="Page Admin" list={AdminPage} icon={AdminPageIcon} />
+        <Resource name="Accueil" list={Home} icon={HomeIcon}/>
+        <Resource name="Machines" list={MachinesList} icon={MachinesIcon}/>
+        <Resource name="Logs" list={LogsList} edit={LogsEdit} create={LogsCreate} icon={LogsIcon}/>
+        <Resource name="Carte" list={Map} icon={MapIcon}/>
+        <Resource name="Statistiques" list={Stats} icon={StatsIcon}/>
+        <Resource name="Page Admin" list={AdminPage} icon={AdminPageIcon}/>
     </Admin>
 );
 
--- src/authProvider.js
+++ src/authProvider.js
@@ -1,33 +1,44 @@
 // in src/authProvider.js
-export default {
+const authProvider = {
     // called when the user attempts to log in
-    login: ({ username }) => {
-        if (username.length > 10) {
-	       return Promise.reject();
-        }
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        return Promise.resolve();
+    login: ({ username, password }) =>  {
+        const request = new Request('https://api.vavf.fr/authentication_token', {
+            method: 'POST',
+            body: JSON.stringify({ email: username, password: password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                localStorage.setItem('token', token);
+            });
     },
     // called when the user clicks on the logout button
     logout: () => {
-        localStorage.removeItem('username');
+        localStorage.removeItem('token');
         return Promise.resolve();
     },
     // called when the API returns an error
     checkError: ({ status }) => {
         if (status === 401 || status === 403) {
-            localStorage.removeItem('username');
+            localStorage.removeItem('token');
             return Promise.reject();
         }
         return Promise.resolve();
     },
     // called when the user navigates to a new location, to check for authentication
     checkAuth: () => {
-        return localStorage.getItem('username')
+        return localStorage.getItem('token')
             ? Promise.resolve()
             : Promise.reject();
     },
     // called when the user navigates to a new location, to check for permissions / roles
     getPermissions: () => Promise.resolve(),
 };
+
+export default authProvider;
